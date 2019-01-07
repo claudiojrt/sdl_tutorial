021b2a6eb3fbf52fa9eb9b974271b47fb02b6e1c
claudiojrt <cj.tondin@hotmail.com>
2019-01-07 12:27:41 -0200

    Loading png files

--- main.cpp
+++ main.cpp
@@ -1,115 +1,72 @@
 #include <iostream>
 #include <string>
 #include <SDL.h>
+#include <SDL_image.h>
 
-const int screenWidht = 1280;
-const int screenHeight = 720;
+int gScreenWidth = 800;
+int gScreenHeight = 600;
 
-SDL_Window *gWindow = NULL;
-SDL_Surface *gScreenSurface = NULL;
-SDL_Surface *gStretchedSurface = NULL;
+SDL_Window* gWindow = NULL;
+SDL_Surface* gSurface = NULL;
 
 bool gQuit = false;
 
-SDL_Surface *loadTexture(std::string path);
-void init();
-void close();
+//Functions
 
-int main(int argc, char *args[])
-{
-    SDL_Event event;
-    SDL_Surface *helloWorld = NULL;
-    SDL_Rect stretchRect;
+SDL_Surface* loadSurface(std::string path);
 
-    bool xDirection = true, yDirection = true;
-    int cX = 35, cY = 49;
+int main(int argc, char* args[]) {
 
-    init();
+    SDL_Init(SDL_INIT_VIDEO);
+    gWindow = SDL_CreateWindow("PNG Loader",SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, gScreenWidth, gScreenHeight, SDL_WINDOW_SHOWN);
+    gSurface = SDL_GetWindowSurface(gWindow);
+    IMG_Init(IMG_INIT_PNG);
 
-    // CARREGAR IMAGEM DE FORMA OTIMIZADA
-    helloWorld = loadTexture("res/helloWorld.bmp");
+    SDL_Surface* pngImage = NULL;
+    pngImage = loadSurface("loaded.png");
+    if (pngImage == NULL){
+        std::cout << "Error";
+    }
 
-    // GAME LOOP
-    while (!gQuit)
-    {
-        while (SDL_PollEvent(&event) != 0)
+    SDL_Event e;
+
+    while(!gQuit) {
+        while( SDL_PollEvent( &e ) != 0 )
         {
-            if (event.type == SDL_QUIT)
+            //User requests quit
+            if( e.type == SDL_QUIT )
             {
                 gQuit = true;
             }
         }
 
-        if (xDirection) {
-            if (cX++ + helloWorld->w > screenWidht) {
-                xDirection = !xDirection;
-            }
-        }
-        else {
-            if (cX-- == 0) {
-                xDirection = !xDirection;
-            }
-        }
-
-        if (yDirection) {
-            if (cY++ + helloWorld->h > screenHeight) {
-                yDirection = !yDirection;
-            }
-        }
-        else {
-            if (cY-- == 0) {
-                yDirection = !yDirection;
-            }
-        }
-
-        stretchRect.x = cX;
-        stretchRect.y = cY;
-        stretchRect.w = helloWorld->w;
-        stretchRect.h = helloWorld->h;
-
-        SDL_BlitScaled( helloWorld, NULL, gScreenSurface, &stretchRect );
-        //SDL_BlitSurface(helloWorld, NULL, gScreenSurface, NULL);
-
+        SDL_BlitScaled(pngImage, NULL, gSurface, NULL);
         SDL_UpdateWindowSurface(gWindow);
-        SDL_Delay(5);
     }
 
-    // FINALIZAÇÃO
-    SDL_FreeSurface(helloWorld);
-    helloWorld = NULL;
+    //Free loaded image
+	SDL_FreeSurface(pngImage);
+	pngImage = NULL;
 
-    close();
+	//Destroy window
+	SDL_DestroyWindow(gWindow);
+	gWindow = NULL;
+    gSurface = NULL;
+
+	//Quit SDL subsystems
+	IMG_Quit();
+	SDL_Quit();
 
     return 0;
 }
 
-void init()
-{
-    SDL_Init(SDL_INIT_VIDEO);
+SDL_Surface* loadSurface(std::string path) {
 
-    gWindow = SDL_CreateWindow("Screen saver", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, screenWidht, screenHeight, SDL_WINDOW_SHOWN);
-    gScreenSurface = SDL_GetWindowSurface(gWindow);
-    SDL_FillRect(gScreenSurface, NULL, SDL_MapRGB(gScreenSurface->format, 255, 255, 255));
-}
+    SDL_Surface* optimizedSurface = NULL;
+    SDL_Surface* loadedSurface = IMG_Load(path.c_str());
 
-void close()
-{
-    SDL_FreeSurface(gStretchedSurface);
-    gStretchedSurface = NULL;
+    optimizedSurface = SDL_ConvertSurface(loadedSurface, gSurface->format, NULL);
+    SDL_FreeSurface(loadedSurface);
 
-    SDL_DestroyWindow(gWindow);
-    gWindow = NULL;
-
-    SDL_Quit();
-}
-
-SDL_Surface *loadTexture(std::string path)
-{
-    SDL_Surface *optimizedTexture = NULL;
-    SDL_Surface *textureLoader = SDL_LoadBMP(path.c_str());
-
-    optimizedTexture = SDL_ConvertSurface(textureLoader, gScreenSurface->format, NULL);
-    SDL_FreeSurface(textureLoader);
-
-    return optimizedTexture;
+    return optimizedSurface;
 }
